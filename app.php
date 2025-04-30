39ff63c7bdcadb10cf91b995c1e9e9bb06a2df71
olivier Rukundo <rr.olivier01@gmail.com>
2025-04-30 22:34:00 +0200

    file changed

--- app.php
+++ app.php
@@ -1,6 +1,10 @@
 <?php
 
-
+// php form to enter name and email address
+if ($_SERVER["REQUEST_mETHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+}
 
 
 ?>
